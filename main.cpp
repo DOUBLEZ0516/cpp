e61dd7c0275b0003c6d363c80cbf930b7f6c57ff
DOUBLEZ0516 <zz1336@nyu.edu>
2019-05-30 01:01:09 +0800

    skeleton

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <memory>
 
-
+#include "queryclass/qclass.h"
 
 using namespace std;
 
--- queryclass/qclass.h
+++ queryclass/qclass.h
@@ -7,10 +7,56 @@
 
 #include <memory>
 #include <map>
-#include <stirng>
+#include <string>
 #include <vector>
 #include <set>
 
 
+
+
+
+
+class QueryResult {
+friend std::ostream& print(const QueryResult&, std::ostream&);
+public:
+	using line_no = std::vector<std::string>::size_type;
+
+	// constructor
+	QueryResult(std::string s,
+				std::shared_ptr<std::set<line_no>> l,
+				std::shared_ptr<std::vector<std::string>> f) :
+				sought(s), lines(l), file(f) {};
+
+	QueryResult() = default;
+	~QueryResult() = default;
+
+
+private:
+	std::string sought;
+	// line_no that sought occurs
+	std::shared_ptr<std::set<line_no>> lines;
+	// input file
+	std::shared_ptr<std::vector<std::string>> file;
+};
+
+
+class TextQuery {
+public:
+	using line_no = std::vector<std::string>::size_type;
+	TextQuery() = default;
+	~TextQuery() = default;
+
+
+	QueryResult query(const std::string&) const;
+
+private:
+	// input file
+	std::shared_ptr<std::vector<std::string>> file;
+	// word : set(line_no)
+	std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
+};
+
+
+std::ostream& print(const QueryResult&, std::ostream&);
 
 #endif 
