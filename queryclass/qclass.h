8cbe6cc1d6ac261ce9afb3f5e2eea5e45e28811f
DOUBLEZ0516 <zz1336@nyu.edu>
2019-05-30 00:31:20 +0800

    update header

---
+++ queryclass/qclass.h
@@ -0,0 +1,16 @@
+#ifndef Q_CLASS_H
+#define Q_CLASS_H
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <memory>
+#include <map>
+#include <stirng>
+#include <vector>
+#include <set>
+
+
+
+#endif 
